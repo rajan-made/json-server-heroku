1163189cd402a482b601b57be4e21204d65c7c84
rajan-made <87546515+rajan-made@users.noreply.github.com>
2021-07-20 04:57:59 +0530

    header update

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 server.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Content-Range', 'posts 0-20/20');
+    res.setHeader('Content-Range', 'bytes 0-10/10');
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
     next();
 });
 
